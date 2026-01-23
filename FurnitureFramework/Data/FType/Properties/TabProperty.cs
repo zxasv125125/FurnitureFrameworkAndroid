38208e10a63b010426d983a4d8b02fdf1409b4ba
zxasv125125 <83066790+zxasv125125@users.noreply.github.com>
2026-01-23 09:34:45 +0700

    Update TabProperty.cs

--- FurnitureFramework/Data/FType/Properties/TabProperty.cs
+++ FurnitureFramework/Data/FType/Properties/TabProperty.cs
@@ -3,33 +3,51 @@ using Microsoft.Xna.Framework.Graphics;
 using Newtonsoft.Json;
 using StardewValley;
 using StardewValley.Menus;
+using System.Collections.Generic;
 
 namespace FurnitureFramework.Data.FType.Properties
 {
-	[JsonConverter(typeof(SpaceRemover<TabProperty>))]
-	public class TabProperty
-	{
-		public string ID;
-		public string HoverText = "";	// useless
-		public string? Condition;
-		public string SourceImage;
-		public Rectangle SourceRect;
+    [JsonConverter(typeof(SpaceRemover<TabProperty>))]
+    public class TabProperty
+    {
+        public string ID;
+        public string HoverText = "";
+        public string? Condition;
+        public string SourceImage;
+        public Rectangle SourceRect;
 
-		public void AddTab(ShopMenu shop_menu, string mod_id, int idx)
-		{
-			shop_menu.tabButtons.Add(
-				new(
-					new Rectangle(0, 0, 64, 64), ModEntry.GetHelper().GameContent.Load<Texture2D>($"FF/{mod_id}/{SourceImage}"), SourceRect, 4f
-				)
-				{
-					myID = 100000 + idx,
-					upNeighborID = -99998,
-					downNeighborID = -99998,
-					rightNeighborID = 3546,
+        public static Dictionary<int, string> ActiveTabConditions = new Dictionary<int, string>();
+
+        public void AddTab(ShopMenu shop_menu, string mod_id, int idx)
+        {
+            int tabID = 100000 + idx;
+
+            if (Condition != null)
+            {
+                if (ActiveTabConditions.ContainsKey(tabID))
+                    ActiveTabConditions[tabID] = Condition;
+                else
+                    ActiveTabConditions.Add(tabID, Condition);
+            }
+
+            shop_menu.tabButtons.Add(
+                new ClickableTextureComponent(
+                    new Rectangle(0, 0, 64, 64), 
+                    ModEntry.GetHelper().GameContent.Load<Texture2D>($"FF/{mod_id}/{SourceImage}"), 
+                    SourceRect, 
+                    4f
+                )
+                {
+                    myID = tabID,
+                    upNeighborID = -99998,
+                    downNeighborID = -99998,
+                    rightNeighborID = 3546,
+                    
+#if !IS_ANDROID
 					Filter = salable => Condition == null || (salable is Item item && GameStateQuery.CheckConditions(Condition, inputItem:item)),
-					hoverText = HoverText	// Does nothing
-				}
-			);
-		}
-	}
+#endif
+                }
+            );
+        }
+    }
 }
